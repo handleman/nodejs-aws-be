ce7526926f398b706d7c6241a0c2fce9a018cdab
Pavel Hendelman <pavel.handleman@gmail.com>
2021-08-25 14:15:35 +0300

    task-3 api gateway

---
+++ product-service/README.md
@@ -0,0 +1,14 @@
+# backend for serverless aws application 
+
+## API endpoints
+
+### list of the products:
+
+https://3rci1wrpaf.execute-api.eu-west-1.amazonaws.com/dev/products
+
+### one specific product by id
+https://3rci1wrpaf.execute-api.eu-west-1.amazonaws.com/dev/products/7567ec4b-b10c-48c5-9345-fc73c48a80aa
+
+`https://3rci1wrpaf.execute-api.eu-west-1.amazonaws.com/dev/products/{id}`
+
+Where `{id}` could be any "id" from the full list of the products.
---
+++ product-service/handler.js
@@ -0,0 +1,28 @@
+'use strict';
+const fs = require('fs').promises;
+const path = require('path');
+
+const { PRODUCTS_MOCK_FILENAME } = require('./constants.js')
+
+const readMockFile = () => fs.readFile(path.resolve(__dirname, `./${PRODUCTS_MOCK_FILENAME}`));
+
+module.exports.getProductsList = async () => {
+  const productsRaw = await readMockFile();
+
+  return {
+    statusCode: 200,
+    body: productsRaw.toString('utf-8'),
+  };
+};
+
+module.exports.getProductById = async (event) => {
+  const {id} = event.pathParameters;
+  const productsRaw = await readMockFile();
+  const products = JSON.parse(productsRaw);
+  const product = products.find((item)=> item.id === id);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(product),
+  };
+};
