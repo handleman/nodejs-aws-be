aff1559a3c3c014b4836423e54465fe4e3fab771
Pavel Hendelman <pavel.handleman@gmail.com>
2021-09-06 12:45:39 +0300

    task-4 get product by id connected to db

--- product-service/getProductById.test.js
+++ product-service/getProductById.test.js
@@ -1,14 +1,15 @@
 const getProductById = require('./getProductById');
 
 test('Product service: getProductsByID()', async () => {
-    const MOCKUP_EVENT_OBJECT = {"pathParameters":{"id":"7567ec4b-b10c-48c5-9345-fc73c48a80aa"}};
+    const MOCKUP_EVENT_OBJECT = { "pathParameters": { "id": "6daf2210-f804-4b33-a686-153c5288c957" } };
     const MOCKUP_ITEM = {
         "count": 4,
         "description": "Mephisto has yet another plan to obtain the Silver Surfer's soul. He disguises himself and walks among the humans. He now comes up with another plot.",
-        "id": "7567ec4b-b10c-48c5-9345-fc73c48a80aa",
-        "price": 2.4,
+        "id": "6daf2210-f804-4b33-a686-153c5288c95",
+        "price": 4,
         "title": "The Silver Surfer",
-        "image": "https://comicvine.gamespot.com/a/uploads/scale_large/11/117763/2403520-ss16.png"
+        "image": "https://comicvine.gamespot.com/a/uploads/scale_large/11/117763/2403520-ss16.png",
+        "count": 200
         }
     const response = await getProductById(MOCKUP_EVENT_OBJECT);
     const {body} = response;
