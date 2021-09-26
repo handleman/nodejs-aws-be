19e1f0d5d5d606ecb8b776857ee00ea34f32f1d3
Pavel Hendelman <pavel.handleman@gmail.com>
2021-09-26 20:28:15 +0300

    task-6  db insert in catalog batch

--- import-service/catalogBatchProcess.js
+++ import-service/catalogBatchProcess.js
@@ -1,12 +1,30 @@
 const AWS = require('aws-sdk');
+const { Client } = require('pg');
+const { dbOptions } = require('./dbOptions');
 
 module.exports = async (event) => {
     const products = event.Records.map(({ body }) => body);
 
     const sns = new AWS.SNS({ region: 'eu-west-1' });
-    //todo: send products to database
 
     try {
+        const client = new Client(dbOptions);
+        await client.connect();
+        for (product of products) {
+            const { title, description, image, price, count } = product;
+
+            const response = await client.query(`
+                insert into product (image, title, description, price) values
+                ('${image}','${title}','${description}',${price}) returning *;
+            `);
+            const { rows } = response;
+            const created = rows[0];
+            const { id } = created;
+            await client.query(`
+                insert into stock (product_id, count) values
+                ('${id}','${count}');
+            `);
+        }
         sns.publish({
             Subject: 'Products were imported successfully',
             Message: JSON.stringify(products),
@@ -22,6 +40,8 @@ module.exports = async (event) => {
             headers,
             body: JSON.stringify(error),
         };
+    } finally {
+        await client.end();
     }
 
 };
