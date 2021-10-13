66ba576df11a6cae75494d1c7ed16c7b72a1d57c
Pavel Hendelman <pavel.handleman@gmail.com>
2021-10-13 20:15:48 +0300

    task-7 typos

--- authorization-service/basicAuthorizer.js
+++ authorization-service/basicAuthorizer.js
@@ -32,19 +32,18 @@ module.exports = (event, context, callback) => {
         const userName = plainCreds[0];
         const password = plainCreds[1];
 
-        console.log(`username: ${username} and password: ${password}`);
+        console.log(`username: ${userName} and password: ${password}`);
 
         const storedPassword = process.env[userName];
         const effect = !storedPassword || storedPassword !== password ? 'Deny' : 'Allow';
 
-        // const iam = new AWS.IAM({ apiVersion: '2010-05-08' });
         const policy = _generatePolicy(encodedCreds, event.methodArn, effect);
 
         callback(null, policy);
 
 
     } catch (error) {
-        callback(`Unauthorized: ${e.message}`);
+        callback(`Unauthorized: ${error.message}`);
     }
 
 };
